049d1aa87977fadabfa53300ddc9152c4cde8520
znguye <dnguyen.jmblc@gmail.com>
2025-05-24 15:12:26 +0200

    added project data

--- app.js
+++ app.js
@@ -39,6 +39,11 @@ app.get('/api/articles', (req, res, next) => {
     res.json(articles)
 })
 
+app.get('/api/projects', (req, res, next) => {
+    console.log(req);
+    res.json(projects)
+})
+
 //404
 app.use((req, res, next) => {
     res.status(404).sendFile(__dirname + '/views/not-found.html')
